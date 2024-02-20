2df44bb4178fea6227dba5838255456ff494db60
Shamsudin <shamsudinov93@bk.ru>
2024-02-20 18:01:31 +0300

    заголовочник окржуности

--- BasicGeometry/circle.h
+++ BasicGeometry/circle.h
@@ -8,11 +8,16 @@ class Circle
     Point o;
     float _radius;
     float _diametr;
+    float _perimeter;
+    float _square;
+    const float PI = 3.14;
 public:
-    Circle();
+    Circle(const Point &,float);
 
     float radius() const;
     float diametr() const;
+    float perimeter() const;
+    float square() const;
 };
 
 #endif // CIRCLE_H
